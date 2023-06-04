122acf9b6da47a05b912be36426fcdd280d25cd1
Miguel Ferreira <1210701@isep.ipp.pt>
2023-06-04 04:34:04 +0100

    upd: minor design changes

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -9,50 +9,50 @@ export default function Welcome(props) {
         name: "",
         email: "",
         password: ""
-      })
+    })
 
-      function handleChange(e) {
+    function handleChange(e) {
         const key = e.target.id;
         const value = e.target.value
         setValues(values => ({
             ...values,
             [key]: value,
         }))
-      }
+    }
 
-      function handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault()
         router.post('/auth/store', values)
-      }
+    }
     return (
         <Guest>
-            <div>
-            <div className="mt-14  bg-white rounded-lg shadow-lg">
-                <div className="grid grid-cols-1 md:grid-cols-2">
-                    <div><img src="/images/create-account-office.jpeg" alt="" /></div>
-                    <div className="flex jusitfy-center items-center w-full">
-                        <form onSubmit={handleSubmit} className="p-8 w-full space-y-5 ">
-                            <div className='form-control'>
-                                <label>Nome</label>
-                                <input id='name' type="text" value={values.name} onChange={handleChange} className="input input-bordered w-full" />
-                                {errors.name && <span className="text-red-500">{ errors.name }</span>}
-                            </div>
-                            <div className='form-control'>
-                                <label>Email</label>
-                                <input id='email' type="text" value={values.email} onChange={handleChange} className="input input-bordered w-full" />
-                                {errors.email && <span className="text-red-500">{ errors.email }</span>}
-                            </div>
-                            <div className='form-control'>
-                                <label>Password</label>
-                                <input id='password' type="password" value={values.password} onChange={handleChange} className="input input-bordered w-full" />
-                                {errors.password && <span className="text-red-500">{ errors.password }</span>}
-                            </div>
+            <div className="w-full flex items-center justify-center">
+                <div className="mt-14  bg-white rounded-lg shadow-lg w-3/4">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div><img src="/images/create-account-office.jpeg" alt="" /></div>
+                        <div className="flex jusitfy-center items-center w-full">
+                            <form onSubmit={handleSubmit} className="p-8 w-full space-y-5 ">
+                                <div className='form-control'>
+                                    <label>Nome</label>
+                                    <input id='name' type="text" value={values.name} onChange={handleChange} className="input input-bordered w-full" />
+                                    {errors.name && <span className="text-red-500">{errors.name}</span>}
+                                </div>
+                                <div className='form-control'>
+                                    <label>Email</label>
+                                    <input id='email' type="text" value={values.email} onChange={handleChange} className="input input-bordered w-full" />
+                                    {errors.email && <span className="text-red-500">{errors.email}</span>}
+                                </div>
+                                <div className='form-control'>
+                                    <label>Password</label>
+                                    <input id='password' type="password" value={values.password} onChange={handleChange} className="input input-bordered w-full" />
+                                    {errors.password && <span className="text-red-500">{errors.password}</span>}
+                                </div>
 
-                            <button type="submit" className='btn mt-5'>Submit</button>
-                        </form>
+                                <button type="submit" className='btn mt-5'>Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </Guest>
     );
